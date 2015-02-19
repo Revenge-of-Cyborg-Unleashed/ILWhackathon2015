628f775c07f5507740c87e65ea0cc63436013ef9
Anthony Gallagher <anthonycgallagher@gmail.com>
2015-02-19 08:40:27 +0000

    Added in fetch_query.py which will do a Skyscanner query, and save the results to the database

---
+++ cgw/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+	name="CGW",
+	version="0.0.1",
+	packages=['cgw','travel'],
+)
---
+++ cgw/travel/fetch_query.py
@@ -0,0 +1,86 @@
+from cgw.skyscanner_query import BrowseCacheQuery
+from travel.models import *
+import importlib.machinery
+
+class saveQuery(object):
+	def __init__(self, origin_place, destination_place, outbound_partial_date, inbound_partial_date, group_name, names_emails):
+		# Declare constants
+		self.MARKET = "GB"
+		self.CURRENCY = "GBP"
+		self.LOCALE = "en-GB"
+	
+		# Declare variables
+		self.origin_place = origin_place
+		self.destination_place = destination_place
+		self.outbound_partial_date = outbound_partial_date
+		self.inbound_partial_date = inbound_partial_date
+	
+		self.name = group_name
+		self.salt = "123123131231" ## TEST VALUE: CHANGE LATER TO HASH ALGORITHM
+	
+		self.names_emails = names_emails
+
+	def doQuery(self):
+		# Get Skyscanner Query
+		self.query = BrowseCacheQuery(market=self.MARKET, currency=self.CURRENCY, locale=self.LOCALE, origin_place=self.origin_place, destination_place=self.destination_place, outbound_partial_date=self.outbound_partial_date, inbound_partial_date=self.inbound_partial_date)
+	
+		# Sort Query by Price
+		self.sortedquotes = self.query.sortQuotesByPrice()
+	
+		# Trim off first 20 results
+		self.sortedqueries = self.sortedquotes[:20]
+	
+		# Format (up to) 20 queries
+		self.final_queries = []
+		for q in self.sortedqueries:
+			#print(q)
+			self.final_queries.append(self.query.formatQuote(q))
+	
+		# Make and fill a new Group
+		group = Group(salt=self.salt, name=self.name)
+		#self.group_id = group.salt
+		group.save()
+		#print(self.group_id)
+	
+		# Make and fill a new Person
+		for (name, email) in self.names_emails:
+			person = Person(name=name, email=email, decided=False, group_id=group)
+			person.save()
+	
+		# Make and fill new Quotes
+		for q in self.final_queries: #CHECK FOR EXISTENCE
+			quote_id = q['QuoteId']
+			price = q['MinPrice']
+			if 'OutboundLeg' in q:
+				dep_date = q['OutboundLeg']['DepartureDate']
+				out_origin = q['OutboundLeg']['OriginName']
+				out_destination = q['OutboundLeg']['DestinationName']
+			else:
+				dep_date = '1970-01-01 00:00+0000'
+				out_origin = ''
+				out_destination=''
+			if 'InboundLeg' in q:
+				ret_date = q['InboundLeg']['DepartureDate']
+				in_origin = q['InboundLeg']['OriginName']
+				in_destination = q['InboundLeg']['DestinationName']
+			else:
+				ret_date = '1970-01-01 00:00+0000'
+				in_origin= ''
+				in_destination= ''
+			direct = q['Direct']
+			quote = Quote(dep_date=dep_date, ret_date=ret_date, direct=direct, price=price, group_id=group, score=0, out_destination=out_destination, in_destination=in_destination, out_origin=out_origin, in_origin=in_origin)
+			quote.save()
+			# Make and fill new Flights
+			if 'OutboundLeg' in q:
+				for carrier in q['OutboundLeg']['CarrierNames']:
+					flight = Flight(quote_id=quote, carrier=carrier, outgoing=True)
+					flight.save()
+			if 'InboundLeg' in q:
+				for carrier in q['InboundLeg']['CarrierNames']:
+					flight = Flight(quote_id=quote, carrier=carrier, outgoing=False)
+					flight.save()
+
+## TEST FUNCTIONS::TO BE DELETED
+q = saveQuery("LON", "ATH", "2015-06-06", "2015-06-08", "Test Group", [("Dave Lead","dave@dave.com"),("Bobby Bobs","bob@bobberty.com")])
+q.doQuery()
+
